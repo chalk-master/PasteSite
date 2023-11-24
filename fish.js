4a3db664b92b24e5882553df7104b0abfbe7fcd3
chalk-master <64747495+chalk-master@users.noreply.github.com>
2023-11-24 23:13:03 +0200

    Fixed bugs

--- fish.js
+++ fish.js
@@ -3,10 +3,10 @@ var hasSubmitted = false;
 function submit() {
     // Remove the placeholder text if it exists
     if (hasSubmitted === false) {
-        document.getElementById("noPastes").remove();
+        document.getElementById("noPastes").innerHTML = "";
     }
 
-    // Create new paste
+    // Create new paste text
     var pasteData = document.getElementById("pasteBox").value;
     var newPaste = document.createElement("p");
     var newPasteText = document.createTextNode(pasteData);
@@ -18,6 +18,11 @@ function submit() {
     deleteBtn.style.marginLeft = "10px"; // REPLACE WITH EXTERNAL CSS FILE LATER
     deleteBtn.onclick = function() {
         newPaste.remove();
+        console.log(document.getElementById('pastes').hasChildNodes());
+        if (!document.getElementById("pastes").hasChildNodes()) {
+            document.getElementById("noPastes").innerHTML = "<em>ur havesing no pastes.. u should make new....</em>";
+            hasSubmitted = false;
+        } 
     };
     newPaste.appendChild(deleteBtn);
 
