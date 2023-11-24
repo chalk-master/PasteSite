4db3e01afbd26a6893af992018ac69553c0fb9f0
chalk-master <64747495+chalk-master@users.noreply.github.com>
2023-11-24 20:23:49 +0200

    Added recent pastes

---
+++ fish.js
@@ -0,0 +1,5 @@
+function submit() {
+    var pasteData = document.getElementById("pasteBox").value;
+    document.getElementById("recentPaste").innerHTML = pasteData;
+    alert("Waow u have sumbiited tekst for java skript");
+}
