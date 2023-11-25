27815b642a9c82ad774cb12fb78b50edff6faf3c
chalk-master <64747495+chalk-master@users.noreply.github.com>
2023-11-25 21:10:01 +0200

    Very slightly upgraded the UI, made the language less goofy. Asks for confirmation when deleting

--- fish.js
+++ fish.js
@@ -1,13 +1,20 @@
 var hasSubmitted = false;
 
 function submit() {
+    // Get the text from the text box. If it's empty, don't create it
+    var pasteData = document.getElementById("pasteBox").value;
+    document.getElementById("pasteBox").value = ""; // Empty the text box after submitting
+    if (pasteData == "") {
+        alert("U troll. U cant haves empty loll")
+        return;
+    }
+
     // Remove the placeholder text if it exists
     if (hasSubmitted === false) {
         document.getElementById("noPastes").innerHTML = "";
     }
 
     // Create new paste text
-    var pasteData = document.getElementById("pasteBox").value;
     var newPaste = document.createElement("p");
     var newPasteText = document.createTextNode(pasteData);
     newPaste.appendChild(newPasteText);
@@ -17,12 +24,13 @@ function submit() {
     deleteBtn.textContent = "Delete";
     deleteBtn.style.marginLeft = "10px"; // REPLACE WITH EXTERNAL CSS FILE LATER
     deleteBtn.onclick = function() {
-        newPaste.remove();
-        console.log(document.getElementById('pastes').hasChildNodes());
-        if (!document.getElementById("pastes").hasChildNodes()) {
-            document.getElementById("noPastes").innerHTML = "<em>ur havesing no pastes.. u should make new....</em>";
-            hasSubmitted = false;
-        } 
+		if (confirm("Are you sure you want to delete this?") === true) {
+			newPaste.remove();
+            if (!document.getElementById("pastes").hasChildNodes()) {
+                document.getElementById("noPastes").innerHTML = "<em>No pastes here...</em>";
+                hasSubmitted = false;
+            }
+		}
     };
     newPaste.appendChild(deleteBtn);
 
@@ -30,6 +38,5 @@ function submit() {
     var firstChild = document.getElementById("pastes").firstChild;
     document.getElementById("pastes").insertBefore(newPaste, firstChild);
 
-    alert("Waow u have sumbiited tekst for java skript");
     hasSubmitted = true;
 }
